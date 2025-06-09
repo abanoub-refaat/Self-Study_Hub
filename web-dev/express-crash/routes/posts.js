8ee50d9ac8427a6cbbb8cb941442f7125552119f
abanoub-refaat <abanoubref3at18@gmail.com>
2025-06-09 20:23:48 +0300

    Implement CRUD operations for posts and add body parser middleware

--- web-dev/express-crash/routes/posts.js
+++ web-dev/express-crash/routes/posts.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { title } from "process";
 const router = express.Router();
 
 let posts = [
@@ -30,6 +31,49 @@ router.get("/:id", (req, res) => {
       .json({ msg: `A post with the id of ${id} was not found` });
   }
   res.status(200).json(post);
+});
+
+// create new post
+router.post("/", (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+  };
+
+  if (!newPost.title) {
+    return res.status(400).json({ msg: "please include a title" });
+  }
+
+  posts.push(newPost);
+
+  console.log(req.body);
+  res.status(201).json(posts);
+});
+
+// update posts
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(404).json({ msg: `no post with id of ${id}` });
+  }
+
+  post.title = req.body.title;
+  res.status(200).json(posts);
+});
+
+// delete post
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(404).json({ msg: `no post with id of ${id}` });
+  }
+
+  posts = posts.filter((post) => post.id !== id);
+  res.status(200).json(posts);
 });
 
 export default router;
